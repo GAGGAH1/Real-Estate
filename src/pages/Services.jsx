9a3410b4c72c4d3622c94980e9a10d7e8b4350f6
GAGGAH1 <titusdcstd2022@gmail.com>
2025-11-08 10:54:11 +0100

    Added objects in the services array with title,desc, and images from my local device

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Image1 from '../assets/images/image1.jpeg';
+import Image2 from '../assets/images/image2.jpeg';
+import Image3 from '../assets/images/image3.jpeg';
+import Image4 from '../assets/images/image4.jpeg';
+import Image5 from '../assets/images/image5.jpeg';
+
 
 const services = [
   {
@@ -15,6 +21,31 @@ const services = [
     title: "Land Spaces",
     desc: "Verified plots and land for development, investment, or agriculture. Secure documentation and advisory support.",
     img: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    title: "Land Spaces",
+    desc: "Verified plots and land for development and secure documentation.",
+    img: Image1
+  },
+  {
+    title: "Land Spaces",
+    desc: "Verified plots and land for development and secure documentation.",
+    img: Image2
+  },
+  {
+    title: "Land Spaces",
+    desc: "Verified plots and land for development and secure documentation.",
+    img: Image3
+  },
+  {
+    title: "Land Spaces",
+    desc: "Verified plots and land for development and secure documentation.",
+    img: Image4
+  },
+  {
+    title: "Residential Properties",
+    desc: "Verified uncompleted house, land for development and secure documentation.",
+    img: Image5
   }
 ];
 
